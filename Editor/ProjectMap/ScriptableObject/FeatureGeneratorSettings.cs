5ef3332ef15ef0ed90e72b05b188a25f4f035d7a
Anton2781618 <69422633+anton-prtikevich@users.noreply.github.com>
2025-11-13 23:49:50 +0300

    обновление

--- Editor/ProjectMap/ScriptableObject/FeatureGeneratorSettings.cs
+++ Editor/ProjectMap/ScriptableObject/FeatureGeneratorSettings.cs
@@ -3,6 +3,10 @@ using UnityEngine;
 
 namespace ModularEventArchitecture.Editor.ProjectMap
 {
+    /// <summary>
+    /// Настройки генератора фич для проектной карты.
+    /// Используется для хранения настроек, таких как ссылки на asmdef по умолчанию.
+    /// </summary>
     [CreateAssetMenu(fileName = "FeatureGeneratorSettings", menuName = "ModularEventArchitecture/Feature Generator Settings")]
     public class FeatureGeneratorSettings : ScriptableObject
     {
